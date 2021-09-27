f676701eccaff8ab249118732486cc1f30c44536
Wusiman Yibulayin <wusiman.yibulayin01@stclairconnect.ca>
2021-09-26 22:46:43 -0400

    Initial Commit

---
+++ Stock Logger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock Logger
+//
+//  Created by Ibrahim Torabek on 2021-09-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
